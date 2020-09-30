3c0cf5681f1a0f8264377d048d93203f75af28c8
LUCAS_LIU <lliu0060@student.monash.edu>
2020-09-30 14:19:50 +1000

    Done 3.4

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,11 +1,13 @@
 package com.example.android.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import timber.log.Timber
 
 
 /**
@@ -25,7 +27,48 @@ class TitleFragment : Fragment() {
 
         setHasOptionsMenu(true)
 
+        Timber.i("onCreateView called")
+
         return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Timber.i("onAttach called")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i("onCreate called")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.i("onViewCreated called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Timber.i("onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Timber.i("onDetach called")
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
